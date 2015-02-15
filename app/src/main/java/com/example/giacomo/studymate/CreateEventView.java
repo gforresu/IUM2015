c12c5c0f6e679e5be6bea5789b2a360b96fe6959
gforresu <giacomo.forresu@gmail.com>
2015-02-15 16:41:11 +0100

    Inserito tutti i picker per le date e per gli orari

--- app/src/main/java/com/example/giacomo/studymate/CreateEventView.java
+++ app/src/main/java/com/example/giacomo/studymate/CreateEventView.java
@@ -5,9 +5,11 @@ import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,12 +19,14 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.SimpleTimeZone;
 
 /**
  * Crea la vista ed i listener per la creazione dell'evento
@@ -32,6 +36,9 @@ public class CreateEventView extends Fragment
     public static final String EVENT_DATE = "event_date";
 
     private EditText endDate;
+    private EditText startDate;
+    private EditText startTime;
+    private EditText endTime;
 
     public CreateEventView()
     {
@@ -41,14 +48,13 @@ public class CreateEventView extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
-
     {
 
         //Cambio titolo all'actionbar
         getActivity().getActionBar().setTitle("Nuovo evento");
 
         //Imposto il listener dal datepicker
-        DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener()
+        DatePickerDialog.OnDateSetListener dateEndPickerListener = new DatePickerDialog.OnDateSetListener()
         {
             public void onDateSet(DatePicker view, int year,int month, int day)
             {
@@ -64,6 +70,62 @@ public class CreateEventView extends Fragment
                 endDate.setText(sel);
             }
     };
+        DatePickerDialog.OnDateSetListener dateStartPickerListener = new DatePickerDialog.OnDateSetListener()
+        {
+            public void onDateSet(DatePicker view, int year,int month, int day)
+            {
+
+
+
+                GregorianCalendar d =new GregorianCalendar(year, month, day);
+
+                SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+
+                String sel = format.format(d.getTime());
+
+                startDate.setText(sel);
+            }
+        };
+        ////////////////////////////////////////////////////
+
+
+        //Imposto i listener del time picker
+        TimePickerDialog.OnTimeSetListener timeStartPickerListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hours, int minutes)
+            {
+                String ora;
+
+                if(minutes < 10)
+                    ora = new String(hours+":"+ "0"+minutes);
+
+                else
+                    ora = new String(hours+":"+ minutes);
+
+                startTime.setText(ora);
+
+            }
+        };
+
+        TimePickerDialog.OnTimeSetListener timeEndPickerListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hours, int minutes)
+            {
+
+                String ora;
+
+                if(minutes < 10)
+                    ora = new String(hours+":"+ "0"+minutes);
+
+                else
+                    ora = new String(hours+":"+ minutes);
+
+                endTime.setText(ora);
+
+            }
+        };
+////////////////////////////////
+
 
         //Recupero la data
         String data = getArguments().getString(EVENT_DATE);
@@ -72,28 +134,86 @@ public class CreateEventView extends Fragment
 
 
         //Imposto la data selezionata nel campo della data inizio dell'evento
-        EditText startDate = (EditText) v.findViewById(R.id.startDate);
+        final EditText tmpStartDate = (EditText) v.findViewById(R.id.startDate);
+        //EditText endDate = (EditText) v.findViewById(R.id.endDate);
+        this.startDate = tmpStartDate;
         startDate.setText(data);
 
 
-        //Picker fragment
-        final EditText endDate = (EditText) v.findViewById(R.id.endDate);
-        this.endDate = endDate;
+        //Picker fragment per la data fine
+        final EditText tmpEndDate = (EditText) v.findViewById(R.id.endDate);
+        this.endDate = tmpEndDate;
 
 
         Calendar cal = Calendar.getInstance();
 
-        final DatePickerDialog dialog = new DatePickerDialog(getActivity(), datePickerListener ,
-                cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH));
 
+        //Definizione dei picker della data
+        final DatePickerDialog endDateDialog = new DatePickerDialog(getActivity(), dateEndPickerListener ,
+                cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH));
 
 
         endDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean b) {
 
-                if(endDate.isFocused())
-                    dialog.show();
+                if(tmpEndDate.isFocused())
+                    endDateDialog.show();
+            }
+        });
+
+
+        final DatePickerDialog startDateDialog = new DatePickerDialog(getActivity(), dateStartPickerListener ,
+                cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH));
+
+
+        startDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+
+                if(tmpStartDate.isFocused())
+                    startDateDialog.show();
+            }
+        });
+
+
+
+        // int hourOfDay, int minute, boolean is24HourView
+
+       /* Calendar start = Calendar.getInstance();
+        start.set(Calendar.HOUR_OF_DAY, 9);
+        start.set(Calendar.MINUTE, 00);*/
+
+
+        //Impostazione dei picker dell'ora
+
+        final TimePickerDialog dialogStartTime = new TimePickerDialog(getActivity(), timeStartPickerListener, 9, 0, true );
+        final TimePickerDialog dialogEndTime = new TimePickerDialog(getActivity(), timeEndPickerListener, 9, 0, true );
+
+        final EditText tmpStartTime = (EditText) v.findViewById(R.id.startTime);
+        this.startTime = tmpStartTime;
+
+        final EditText tmpEndTime = (EditText) v.findViewById(R.id.endTime);
+        this.endTime = tmpEndTime;
+
+        startTime.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+
+                if(tmpStartTime.isFocused())
+                    dialogStartTime.show();
+
+            }
+        });
+
+
+        endTime.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+
+                if(tmpEndTime.isFocused())
+                    dialogEndTime.show();
+
             }
         });
 
